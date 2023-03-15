83c43bfcb580e6476d46a35d4b9c07bba4e866c7
PatLuo <pluon099@uottawa.ca>
2023-03-15 19:55:46 -0400

    init project

--- express_backend/app.js
+++ express_backend/app.js
@@ -1,24 +1,20 @@
-const express = require("express");
-
+import express from "express";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://lqcpsjtrtdswgmibrugn.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxY3BzanRydGRzd2dtaWJydWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxMzgxODksImV4cCI6MTk5MzcxNDE4OX0.ZD-Z8tIZw3FMDzME0WhbJnMcHXxVvbhMU1znFnPjNXU";
+const supabase = createClient(supabaseUrl, supabaseKey); //initialize supabase
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-const app = express();
 const port = 3000;
+const app = express(); //initialize express
 
+//routes
 app.get("/", async (req, res) => {
-  //send a query to the database
+  let { data: hotel_chain, error } = await supabase.from("hotel_chain").select("*");
 
-  let { data: hotel_chain, error } = await supabase.from("hotel_chain").select("id");
-  if (error) {
-    console.log(error);
-  }
-  res.send(hotel_chain);
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(hotel_chain);
 });
 
 //starts the server
