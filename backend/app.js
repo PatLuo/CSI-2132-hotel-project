68e5cdb10c3c65d28dd7092cff0131504ca66b45
PatLuo <patrick.luong615@gmail.com>
2023-03-27 19:09:30 -0400

    switch to pg

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
-import express from "express";
-import { createClient } from "@supabase/supabase-js";
+const express = require("express"); //import express
+const pg = require("pg"); //import postgres
 
-const supabaseUrl = "https://lqcpsjtrtdswgmibrugn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxY3BzanRydGRzd2dtaWJydWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxMzgxODksImV4cCI6MTk5MzcxNDE4OX0.ZD-Z8tIZw3FMDzME0WhbJnMcHXxVvbhMU1znFnPjNXU";
-const supabase = createClient(supabaseUrl, supabaseKey); //initialize supabase
+const client = new pg.Client("postgres://xwswtjih:belDdHL2W3NQZbRMP-E3VQ5j17T_ijdt@isilo.db.elephantsql.com/xwswtjih"); //connect to database
+
+client.connect((err) => {
+  if (err) {
+    return console.error("could not connect to postgres", err);
+  }
+  console.log("successfully connected to postgres");
+});
 
 const port = 3000;
 const app = express(); //initialize express
@@ -15,12 +19,30 @@ app.use((req, res, next) => {
   next();
 });
 
+//queries
+const getChains = "SELECT * FROM hotel_chain";
+const addChain = "INSERT INTO hotel_chain VALUES (5, 8)";
+const getRooms = "SELECT * FROM room";
+const clearTable = "DELETE FROM hotel_chain";
+
 //routes
 app.get("/", async (req, res) => {
-  let { data: hotel_chain, error } = await supabase.from("hotel_chain").select("*");
+  client.query(getChains, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log("query successful");
+    res.json(result.rows);
+  });
+});
 
-  if (error) return res.status(500).json({ error: error.message });
-  res.json(hotel_chain);
+app.get("/addChain", async (req, res) => {
+  client.query(addChain, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log("query successful");
+  });
 });
 
 //starts the server
