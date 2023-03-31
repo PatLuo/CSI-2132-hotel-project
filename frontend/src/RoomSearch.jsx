6b2fe0ca5d055560900ec2da09910b1e9a9a7039
PatLuo <patrick.luong615@gmail.com>
2023-03-31 01:21:31 -0400

    create room search component

---
+++ frontend/src/RoomSearch.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import axios from "axios";
+
+function RoomSearch() {
+  return (
+    <div>
+      <h1>Room Search</h1>
+      <input type="text" className="form-contrl" placeholder />
+    </div>
+  );
+}
