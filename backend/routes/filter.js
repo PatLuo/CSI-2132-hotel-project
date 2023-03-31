2a2740e480df9f0cbdbfc9eba6e59a096e66086a
PatLuo <patrick.luong615@gmail.com>
2023-03-31 17:22:11 -0400

    finish room searching

--- backend/routes/filter.js
+++ backend/routes/filter.js
@@ -3,11 +3,38 @@ const router = express.Router();
 
 const client = require("../db");
 
+router.get("/all", async (req, res) => {
+  const query = `SELECT room_id FROM room;`;
+  client.query(query, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    res.json(result.rows);
+  });
+});
+
+router.get("/date/:searchValue", async (req, res) => {
+  const searchValue = req.params.searchValue.split(",");
+  const query = `
+  SELECT room_id
+  FROM room r 
+  WHERE r.room_id NOT IN (SELECT room_id 
+    FROM booking_renting b 
+    WHERE (b.start_date, b.end_date) OVERLAPS ('${searchValue[0]}', '${searchValue[1]}'));`;
+  client.query(query, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows.length);
+    res.json(result.rows);
+  });
+});
+
 router.get("/capacity/:searchValue", async (req, res) => {
   const query = `
   SELECT room_id
   FROM room
-  WHERE capacity >= ${req.params.searchValue};`;
+  WHERE capacity = ${req.params.searchValue};`;
   client.query(query, (err, result) => {
     if (err) {
       return console.error("error running query", err);
@@ -22,6 +49,50 @@ router.get("/area/:searchValue", async (req, res) => {
     SELECT room_id
     FROM hotel JOIN room USING (hotel_id, chain_id)
     WHERE city = '${searchValue}';`;
+  client.query(query, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    res.json(result.rows);
+  });
+});
+
+router.get("/chain/:searchValue", async (req, res) => {
+  const query = `
+  SELECT room_id
+  FROM hotel JOIN room USING (hotel_id, chain_id)
+  WHERE chain_id = ${req.params.searchValue};
+  `;
+  client.query(query, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    res.json(result.rows);
+  });
+});
+
+router.get("/category/:searchValue", async (req, res) => {
+  const query = `
+    SELECT room_id
+FROM hotel JOIN room USING (hotel_id, chain_id)
+WHERE categorization = ${req.params.searchValue};
+ 
+    `;
+  client.query(query, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    res.json(result.rows);
+  });
+});
+
+router.get("/numRooms/:searchValue", async (req, res) => {
+  const query = `
+    SELECT room_id
+    FROM hotel JOIN room USING (hotel_id, chain_id)
+    WHERE number_of_rooms >= ${req.params.searchValue};
+   
+      `;
   client.query(query, (err, result) => {
     if (err) {
       return console.error("error running query", err);
--- frontend/src/RoomSearch.jsx
+++ frontend/src/RoomSearch.jsx
@@ -4,6 +4,9 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const RoomSearch = () => {
+  const url = "http://localhost:3000/filter";
+
+  //search criteria
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [capacity, setCapacity] = useState("");
@@ -13,33 +16,106 @@ const RoomSearch = () => {
   const [numRooms, setNumRooms] = useState("");
   const [roomPrice, setRoomPrice] = useState("");
 
-  const [filteredByDate, setFilteredByDate] = useState([]);
-  const [filteredByCapacity, setFilteredByCapacity] = useState([]);
-  const [filteredByArea, setFilteredByArea] = useState([]);
-  const [filteredByHotelChain, setFilteredByHotelChain] = useState([]);
-  const [filteredByHotelCategory, setFilteredByHotelCategory] = useState([]);
-  const [filteredByNumRooms, setFilteredByNumRooms] = useState([]);
-  const [filteredByRoomPrice, setFilteredByRoomPrice] = useState([]);
+  //arrays containing rooms ids that match the search criteria
+  let filteredByDate = [];
+  let filteredByCapacity = [];
+  let filteredByArea = [];
+  let filteredByHotelChain = [];
+  let filteredByHotelCategory = [];
+  let filteredByNumRooms = [];
+  let filteredByRoomPrice = [];
 
-  const handleSearch = (filterCategory) => {
-    axios.get("http://localhost:3000/filter").then((res) => {
-      setData(res.data);
+  //array containing the intersection of the above arrays
+  const [intersection, setIntersection] = useState([]);
+
+  useEffect(() => {
+    console.log(intersection);
+  }, [intersection]);
+
+  const searchByDate = async () => {
+    const range = startDate.toLocaleDateString().replace(/\//g, "-") + "," + endDate.toLocaleDateString().replace(/\//g, "-");
+    await axios.get(`${url}/date/${range}`).then((res) => {
+      filteredByDate = res.data.map((x) => x.room_id);
     });
+  };
+
+  const searchByCapacity = async () => {
+    if (capacity !== "") {
+      await axios.get(`${url}/capacity/${capacity}`).then((res) => {
+        filteredByCapacity = res.data.map((x) => x.room_id);
+      });
+    }
+  };
+
+  const searchByArea = async () => {
+    if (area !== "") {
+      await axios.get(`${url}/area/${area}`).then((res) => {
+        filteredByArea = res.data.map((x) => x.room_id);
+      });
+    }
+  };
+
+  const searchByChain = async () => {
+    if (hotelChain !== "") {
+      await axios.get(`${url}/chain/${hotelChain}`).then((res) => {
+        filteredByHotelChain = res.data.map((x) => x.room_id);
+      });
+    }
+  };
+
+  const searchByCategory = async () => {
+    if (hotelCategory !== "") {
+      await axios.get(`${url}/category/${hotelCategory}`).then((res) => {
+        filteredByHotelCategory = res.data.map((x) => x.room_id);
+      });
+    }
+  };
+
+  const searchByNumRooms = async () => {
+    if (numRooms !== "") {
+      await axios.get(`${url}/numRooms/${numRooms}`).then((res) => {
+        filteredByNumRooms = res.data.map((x) => x.room_id);
+      });
+    }
+  };
+
+  const searchByPrice = async () => {
+    if (roomPrice !== "") {
+      await axios.get(`${url}/price/${roomPrice}`).then((res) => {
+        filteredByRoomPrice = res.data.map((x) => x.room_id);
+      });
+    }
+  };
+
+  const handleSearch = async () => {
+    await searchByDate();
+    await searchByCapacity();
+    await searchByArea();
+    await searchByChain();
+    await searchByCategory();
+    await searchByNumRooms();
+    await searchByPrice();
+
+    const filteredArrays = [
+      filteredByDate,
+      filteredByCapacity,
+      filteredByArea,
+      filteredByHotelChain,
+      filteredByHotelCategory,
+      filteredByNumRooms,
+      filteredByRoomPrice,
+    ];
+    const nonEmptyArrays = filteredArrays.filter((x) => x.length > 0);
+    const roomsInAll = nonEmptyArrays.reduce((accumulator, currentArray) => {
+      return accumulator.filter((element) => currentArray.includes(element));
+    });
+    setIntersection(roomsInAll);
   };
 
   return (
     <div>
       <h1>Room Search</h1>
-      <ul>
-        <li>{startDate.toLocaleDateString().replace(/\//g, "-")}</li>
-        <li>{endDate.toLocaleDateString().replace(/\//g, "-")}</li>
-        <li>{capacity}</li>
-        <li>{area}</li>
-        <li>{hotelChain}</li>
-        <li>{hotelCategory}</li>
-        <li>{numRooms}</li>
-        <li>{roomPrice}</li>
-      </ul>
+
       <label htmlFor="startDate">Start date:</label>
       <DatePicker
         selected={startDate}
@@ -63,32 +139,35 @@ const RoomSearch = () => {
 
       <label htmlFor="capacity">Capacity:</label>
       <select className="form-control" value={capacity} onChange={(event) => setCapacity(event.target.value)}>
+        <option value="">Select</option>
         <option value="1">1 Person</option>
         <option value="2">2 Person</option>
         <option value="3">3 Person</option>
         <option value="4">4 Person</option>
         <option value="5">5 Person</option>
       </select>
+
       <label htmlFor="area">Area:</label>
       <input className="form-control" type="text" value={area} onChange={(event) => setArea(event.target.value)} />
 
       <label htmlFor="hotelChain">Hotel chain:</label>
       <select className="form-control" value={hotelChain} onChange={(event) => setHotelChain(event.target.value)}>
-        <option value="">-- Select --</option>
-        <option value="chain1">Chain 1</option>
-        <option value="chain2">Chain 2</option>
-        <option value="chain3">Chain 3</option>
-        <option value="chain4">Chain 4</option>
-        <option value="chain5">Chain 5</option>
+        <option value="">Select</option>
+        <option value="1">Chain 1</option>
+        <option value="2">Chain 2</option>
+        <option value="3">Chain 3</option>
+        <option value="4">Chain 4</option>
+        <option value="5">Chain 5</option>
       </select>
 
       <label htmlFor="hotelCategory">Hotel category:</label>
       <select className="form-control" value={hotelCategory} onChange={(event) => setHotelCategory(event.target.value)}>
-        <option value="">1 Star</option>
-        <option value="category2">2 Star</option>
-        <option value="category3">3 Star</option>
-        <option value="category3">4 Star</option>
-        <option value="category3">4 Star</option>
+        <option value="">Select</option>
+        <option value="1">1 Star</option>
+        <option value="2">2 Star</option>
+        <option value="3">3 Star</option>
+        <option value="4">4 Star</option>
+        <option value="5">5 Star</option>
       </select>
 
       <label htmlFor="numRooms">Number of rooms:</label>
@@ -98,6 +177,11 @@ const RoomSearch = () => {
       <input className="form-control" type="number" value={roomPrice} onChange={(event) => setRoomPrice(event.target.value)} />
 
       <button onClick={handleSearch}>Search</button>
+      {intersection.length > 0 && (
+        <div>
+          <h2>Results</h2>
+        </div>
+      )}
     </div>
   );
 };
